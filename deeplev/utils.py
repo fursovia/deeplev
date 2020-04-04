c4c0264290fbcf597f566cb8cf8caa8682b1514c
fursovia <fursov.ia@gmail.com>
2020-04-04 18:21:32 +0300

    FIX: removed wer calculation

--- deeplev/utils.py
+++ deeplev/utils.py
@@ -13,19 +13,3 @@ def load_weights(model: Model, path: str, location: str = 'cpu') -> None:
 @functools.lru_cache(maxsize=500)
 def edit_distance(sequence_a: str, sequence_b: str) -> float:
     return lvs.distance(sequence_a, sequence_b)
-
-
-@functools.lru_cache(maxsize=500)
-def calculate_wer(sequence_a: str, sequence_b: str) -> float:
-    # taken from https://github.com/SeanNaren/deepspeech.pytorch/blob/master/decoder.py
-    b = set(sequence_a.split() + sequence_b.split())
-    word2char = dict(zip(b, range(len(b))))
-
-    w1 = [chr(word2char[w]) for w in sequence_a.split()]
-    w2 = [chr(word2char[w]) for w in sequence_b.split()]
-
-    return lvs.distance(''.join(w1), ''.join(w2))
-
-
-def calculate_normalized_wer(sequence_a: str, sequence_b: str) -> float:
-    return calculate_wer(sequence_a, sequence_b) / max(len(sequence_a.split()), len(sequence_b.split()))
