c189709c8ff3edaeadf1c18a17c0a762dd0d7abd
fursovia <fursov.ia@gmail.com>
2020-04-05 14:10:21 +0300

    FIX: seq2seq_encoder is optional

--- deeplev/model.py
+++ deeplev/model.py
@@ -25,8 +25,8 @@ class DeepLevenshtein(Model):
     def __init__(self,
                  vocab: Vocabulary,
                  text_field_embedder: TextFieldEmbedder,
-                 seq2seq_encoder: Seq2SeqEncoder,
                  seq2vec_encoder: Seq2VecEncoder,
+                 seq2seq_encoder: Optional[Seq2SeqEncoder] = None,
                  attention: Optional[Attention] = None) -> None:
         super().__init__(vocab)
         self.text_field_embedder = text_field_embedder
@@ -66,10 +66,19 @@ class DeepLevenshtein(Model):
     def encode_sequence(self, sequence: Dict[str, torch.LongTensor]) -> Dict[str, torch.Tensor]:
         embedded_sequence = self.text_field_embedder(sequence)
         mask = util.get_text_field_mask(sequence).float()
-        embedded_sequence_matrix = self.seq2seq_encoder(embedded_sequence, mask=mask)
-        embedded_sequence_vector = self.seq2vec_encoder(embedded_sequence_matrix, mask=mask)
-        output = {'mask': mask, 'vector': embedded_sequence_vector, 'matrix': embedded_sequence_matrix}
-        return output
+        # It is needed if we pad the initial sequence
+        mask = torch.nn.functional.pad(
+            mask,
+            pad=[0, embedded_sequence.size(1) - mask.size(1)]
+        )
+        if self.seq2seq_encoder is not None:
+            embedded_sequence = self.seq2seq_encoder(embedded_sequence, mask=mask)
+        embedded_sequence_vector = self.seq2vec_encoder(embedded_sequence, mask=mask)
+        return {
+            'mask': mask,
+            'vector': embedded_sequence_vector,
+            'matrix': embedded_sequence
+        }
 
     def forward(self,
                 sequence_a: Dict[str, torch.LongTensor],
