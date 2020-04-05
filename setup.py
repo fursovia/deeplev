e8ee917ca49a436420538e3adf006e3fd2e677a5
idenra <denrazv@mail.ru>
2020-04-05 22:56:49 +0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='deeplev',
+    version='0.0',
+    description='Deep Learning implementation of Levenshtein distance.',
+    license='MIT',
+    packages=['deeplev'],
+    zip_safe=False,
+)
