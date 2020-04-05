a0fbf4279a7d37a1173b32925b5aa5cf6f377549
fursovia <fursov.ia@gmail.com>
2020-04-05 13:40:08 +0300

    ADD: one_hot embedder

---
+++ deeplev/allennlp_modules/onehot_embedder.py
@@ -0,0 +1,29 @@
+import torch
+
+from allennlp.modules.token_embedders import TokenEmbedder
+
+
+class OnehotEmbedder(TokenEmbedder):
+    def __init__(self, vocab_size: int, max_seq_length: int) -> None:
+        super().__init__()
+        self._vocab_size = vocab_size
+        self._max_seq_length = max_seq_length
+
+    def get_output_dim(self) -> int:
+        return self._vocab_size
+
+    def _one_hot(self, tensor: torch.Tensor) -> torch.Tensor:
+        return torch.nn.functional.one_hot(tensor, self._vocab_size)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        num_tokens = inputs.size(1)
+        one_hot_labels = self._one_hot(inputs)
+        if num_tokens >= self._max_seq_length:
+            return one_hot_labels[:, :self._max_seq_length, :]
+
+        pad_length = self._max_seq_length - num_tokens
+        one_hot_labels = torch.nn.functional.pad(
+            one_hot_labels,
+            pad=[0, 0, 0, pad_length]
+        )
+        return one_hot_labels
