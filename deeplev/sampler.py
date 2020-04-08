8887b3fabec9ed8d9db2cdd5624239ed82882e45
gridav <grigor.dvd@gmail.com>
2020-04-08 05:03:37 +0300

    create dataset now uses triplets

---
+++ deeplev/sampler.py
@@ -0,0 +1,33 @@
+from typing import Sequence, Optional
+from abc import ABC, abstractmethod
+import random
+
+from deeplev.utils import clean_sequence
+from deeplev.typo_generator import generate_default_typo
+
+
+class BaseSampler:
+    def __init__(self, texts: Sequence[str]) -> None:
+        self._texts = texts
+
+    @abstractmethod
+    def sample_positive(self, text: Optional[str] = None) -> str:
+        pass
+
+    @abstractmethod
+    def sample_negative(self, text: Optional[str] = None) -> str:
+        pass
+
+
+class TypoSampler(BaseSampler):
+    def __init__(self, texts: Sequence[str]) -> None:
+        texts = [clean_sequence(x) for x in texts]
+        super().__init__(texts)
+
+        self._vocab = list(set(''.join(texts)))
+
+    def sample_positive(self, text: Optional[str] = None) -> str:
+        return generate_default_typo(text, self._vocab)
+
+    def sample_negative(self, text: Optional[str] = None) -> str:
+        return random.sample(self._texts, 1)[0]
--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -1,21 +1,20 @@
 import argparse
+import random
 from tqdm import tqdm
 from pathlib import Path
 
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 
-from deeplev.utils import edit_distance, clean_sequence
-from deeplev.typo_generator import generate_default_typo
+from deeplev.utils import edit_distance
+from deeplev.sampler import TypoSampler
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--csv_path', type=str, required=True)
 parser.add_argument('--col_name', type=str, required=True)
 parser.add_argument('--output_dir', type=str, required=True)
-parser.add_argument('--num_dissimilar', type=int, default=300000)
-parser.add_argument('--num_similar', type=int, default=700000)
+parser.add_argument('--dataset_size', type=int, default=1_000_000)
 parser.add_argument('--test_size', type=float, default=0.05)
 
 
@@ -31,30 +30,24 @@ if __name__ == '__main__':
     data = data[~data[args.col_name].isna()]
     # TODO: drop zero-len examples
     sequences = data[args.col_name].astype(str).tolist()
-    sequences = list(map(clean_sequence, sequences))
-    vocab = list(set(''.join(sequences)))
-
-    dissimilar_examples = []
-    dissimilar_indexes = np.random.randint(0, len(sequences), size=(args.num_dissimilar, 2))
-    for id1, id2 in tqdm(dissimilar_indexes):
-        tr1 = sequences[id1]
-        tr2 = sequences[id2]
-        dist = edit_distance(tr1, tr2)
-        dissimilar_examples.append((tr1, tr2, dist))
-
-    similar_examples = []
-    similar_indexes = np.random.randint(0, len(sequences), size=args.num_similar)
-    for idx in tqdm(similar_indexes):
-        tr1 = sequences[idx]
-        tr2 = generate_default_typo(tr1, vocab)
-        if tr1 != tr2:
-            dist = edit_distance(tr1, tr2)
-            similar_examples.append((tr1, tr2, dist))
+    sampler = TypoSampler(sequences)
 
     examples = []
-    examples.extend(dissimilar_examples)
-    examples.extend(similar_examples)
-    examples = pd.DataFrame(examples, columns=['sequence_a', 'sequence_b', 'distance'])
+
+    for i in tqdm(range(args.dataset_size)):
+        random_text = random.sample(sequences, 1)[0]
+        positive = sampler.sample_positive(random_text)
+        negative = sampler.sample_negative(random_text)
+
+        if positive == random_text or negative == random_text:
+            continue
+
+        positive_distance = edit_distance(random_text, positive)
+        negative_distance = edit_distance(random_text, negative)
+
+        examples.append((random_text, positive, negative, positive_distance, negative_distance))
+
+    examples = pd.DataFrame(examples, columns=['anchor', 'positive', 'negative', 'positive_distance', 'negative_distance'])
 
     train, test = train_test_split(examples, test_size=args.test_size)
     train.to_csv(train_path, index=False)
