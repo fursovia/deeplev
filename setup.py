4653628f3d1c1d8f90243323972fc254f30c6bc7
idenra <denrazv@mail.ru>
2020-04-10 20:32:31 +0500

    version in setup.py is updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='deeplev',
-    version='0.0',
+    version='0.1.0',
     description='Deep Learning implementation of Levenshtein distance.',
     license='MIT',
     packages=['deeplev'],
