61f6e4f3a7c636e0c495ba93b25523912d285800
fursovia <fursov.ia@gmail.com>
2020-04-11 15:45:46 +0300

    ADD: OnehotCNN with new from_params method

---
+++ deeplev/allennlp_modules/onehot_cnn.py
@@ -0,0 +1,17 @@
+from allennlp.common import Params
+from allennlp.data import Vocabulary
+from allennlp.modules.seq2vec_encoders import CnnEncoder, Seq2VecEncoder
+
+
+@Seq2VecEncoder.register("onehot_cnn")
+class OnehotCnnEncoder(CnnEncoder):
+    @classmethod
+    def from_params(cls, params: Params, vocab: Vocabulary) -> "OnehotCnnEncoder":
+        # is not None if you now vocab_size in advance
+        embedding_dim = params.pop_int("embedding_dim", vocab.get_vocab_size("tokens"))
+        num_filters = params.pop_int("num_filters")
+        ngram_filter_sizes = params.pop("ngram_filter_sizes", (2, 3, 4, 5))
+
+        params.assert_empty(cls.__name__)
+
+        return cls(embedding_dim=embedding_dim, num_filters=num_filters, ngram_filter_sizes=ngram_filter_sizes)
