1ae6720a1ea9a68848f3369e661cef2c841bec0b
fursovia <fursov.ia@gmail.com>
2020-06-07 02:57:47 +0300

    FIX: replaced tests with newer versions

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+import pathlib
+
+PROJECT_ROOT = (pathlib.Path(__file__).parent / "..").resolve()
+TESTS_ROOT = PROJECT_ROOT / "tests"
+FIXTURES_ROOT = TESTS_ROOT / "fixtures"
--- tests/dataset_test.py
+++
@@ -1,6 +0,0 @@
-from deeplev.dataset import LevenshteinReader
-
-
-def test_dummy_dataset():
-    reader = LevenshteinReader()
-    assert reader is not None
