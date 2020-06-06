d52e11dfe660d136b948492ba828ae8bba50f12c
fursovia <fursov.ia@gmail.com>
2020-06-07 02:55:02 +0300

    FIX: moved and refactored predictors

--- deeplev/predictors/__init__.py
+++ deeplev/predictors/__init__.py
@@ -1,4 +1,3 @@
-from .predictor import DeepLevenshteinPredictor
 from .distance_predictor import DistancePredictor
 from .embedder_predictor import EmbedderPredictor
 from .search_predictor import SearchPredictor
